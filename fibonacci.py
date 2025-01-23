94d7cdf65b4fc6b0ff46423e1b336ab272a69d97
Matthias Braun <167146329+matt4braun@users.noreply.github.com>
2025-01-23 18:07:56 -0500

    Update fibonacci.py
    adding comments

--- fibonacci.py
+++ fibonacci.py
@@ -2,8 +2,8 @@ n = int(input("input n"))
 
 def fibonacci(n):
     if n>2:
-        return fibonacci(n-1)+fibonacci(n-2)
+        return fibonacci(n-1)+fibonacci(n-2)        #repeat the function with the 2 previous numbers
     else:
-        return 1
+        return 1    #when done return 1 (the final number)
 
 print(fibonacci(n))
